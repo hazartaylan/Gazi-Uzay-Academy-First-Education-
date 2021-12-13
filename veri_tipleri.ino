0b58aa64b84e26d9c50b118243eca9a7349e396f
hazartaylan <57994196+hazartaylan@users.noreply.github.com>
2021-12-14 01:02:51 +0300

    Create veri_tipleri.ino

---
+++ veri_tipleri.ino
@@ -0,0 +1,44 @@
+//Boolean değişken tipimiz 1 bitlik veri saklama kapasitesine sahip. Bu veri 1 veya 0 olabileceği gibi True (Doğru) veya False (Yanlış) olabilir.
+bool sayi = true;
+bool sayi1 = false;
+//ya da 
+bool sayi = 1;
+bool sayi1 = 0;
+//*******************//
+//Karakter türü değişken tipidir. Bir byte yani 8 bitlik unicode karakter tipinde veri saklar. İşaretli -127 ve 127 arasında, işaretsiz 0 ve 255 arasında sayı tutabilir.
+char karakter = 'a';
+char karakter2 = 'B';
+char karakter3 = 99;
+//*****************//
+//Sayı türü değişken tipidir. Bir byte yani 8 bit işaretsiz tam sayı türünden verileri tutabilir. 0 ile 255 arasındaki sayıları tutmak için kullanılır.
+byte sayi = 0;
+byte sayi2 = 128;
+byte sayi3 = 255;
+//*****************//
+//Tamsayı türü değişken tipidir. Bir iki byte yani 16 bit işaretli tam sayı türünden verileri tutabilir. -32,768 ile 32,767 arasındaki sayıları tutmak için kullanılır.
+int sayi = 0;
+int sayi2 = -15780;
+int sayi3 = 65535;
+//******************//
+//Sayı türü değişken tipidir. Bir iki byte yani 16 bit işaretsiz tam sayı türünden verileri tutabilir. 0 ile 65535 arasındaki sayıları tutmak için kullanılır.
+unsigned int sayi = 0;
+unsigned int sayi2 = 15780;
+unsigned int sayi3 = 65535;
+//******************//
+//Sayı türü değişken tipidir. 32 bit ondalıklı sayı türleri için kullanılır. -3.4028235E+38 ile 3.4028235E+38 arasındaki sayıları tutabilir.
+float sayi = 0;
+float sayi2 = -1.5780;
+float sayi3 = 65.535;
+//******************//
+//Sayı türü değişken tipidir. 32 bit ondalıklı sayı türleri için kullanılır. Kullanımı float ile aynıdır.
+double sayi = 0;
+double sayi2 = -1.5780;
+double sayi3 = 65.535;
+//*****************//
+//Karakter türünde bir değişkendir. Kapsamı bellek boyutu ile sınırlıdır.
+String metin = "Bu bir strin türü değişkendir";
+void setup() {  
+}
+
+void loop() { 
+}
